abc02520eeecc6546e7adbd219eeb3e84db2f61f
Arpa <arpa.banerjee.uttarpara@gmail.com>
2021-06-13 00:31:01 +0530

    checked in auth and pagenotfound folder

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { AuthRoutingModule } from './auth-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    SignUpComponent,
+    SignInComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
